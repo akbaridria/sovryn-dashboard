c24aaf4663576de9b296c62a33ad0eddd0389c7f
akbaridria <akbaridria15@gmail.com>
2021-07-12 16:06:53 +0700

    Fix Bot and add new endpoint

--- bot-sovryn/main.py
+++ bot-sovryn/main.py
@@ -8,26 +8,36 @@ from utils import utils
 from utils import database
 from modular import borrow
 from modular import lending
+from datetime import date, datetime
 
 API_KEY = config('API_KEY')
-block_swap = 2742574
-block_borrow = 2742496
-block_lend_mint = 2742512
-block_lend_burn = 2744165
 
 while True :
-    # _swap = swap.swap(API_KEY,block_swap)
-    # _r = _swap.crawling_data()
-    # block_swap = _swap.process_data(_r)
-    # print(block_swap)
-    _borrow = borrow.borrow(API_KEY, block_borrow)
-    _r_borrow = _borrow.call_api_covalent()
-    block_borrow = _borrow.process_data(_r_borrow)
-    # _lending = lending.lending(API_KEY)
-    # _r_lending_mint = _lending.call_api_covalent_mint(block_lend_mint)
-    # sleep(1)
-    # _r_lending_burn = _lending.call_api_covalent_burn(block_lend_burn)
-    # block_lend_mint, block_lend_burn = _lending.process_data(_r_lending_mint, _r_lending_burn, block_lend_mint, block_lend_burn, API_KEY)
-    
+    _d = database.database()
+    today = date.today()
+    block_swap = _d.getBlock('swap')
+    block_borrow = _d.getBlock('borrowing')
+    block_lend_mint = _d.getBlockLend('lending', 'Mint')
+    block_lend_burn = _d.getBlockLend('lending', 'Burn')
+    date_swap = _d.getDate('swap')
+    date_borrow = _d.getDate('borrowing')
+    date_lend_mint = _d.getDateLend('lending', 'Mint')
+    date_lend_burn = _d.getDateLend('lending', 'Burn')
+    if date_swap["date"] < today :
+        _swap = swap.swap(API_KEY,int(block_swap["block"]))
+        _r = _swap.crawling_data()
+        block_swap = _swap.process_data(_r)
+        print(block_swap)
+    if date_borrow["date"] < today :
+        _borrow = borrow.borrow(API_KEY, int(block_borrow["block"]))
+        _r_borrow = _borrow.call_api_covalent()
+        block_borrow = _borrow.process_data(_r_borrow)
+    if date_lend_burn["date"] < today and date_lend_mint["date"] < today :
+        _lending = lending.lending(API_KEY)
+        _r_lending_mint = _lending.call_api_covalent_mint(int(block_lend_mint["block"]))
+        sleep(1)
+        _r_lending_burn = _lending.call_api_covalent_burn(int(block_lend_burn["block"]))
+        block_lend_mint, block_lend_burn = _lending.process_data(_r_lending_mint, _r_lending_burn, int(block_lend_mint["block"]), int(block_lend_burn["block"]), API_KEY)
+        
     
     sleep(3)
